f7e95aeea8881ada58c5cccd5934b116fcbd182a
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2013-01-20 21:42:52 +0100

    implements num

--- lib/decimal.dart
+++ lib/decimal.dart
@@ -17,6 +17,10 @@ library decimal;
 import 'package:meta/meta.dart';
 
 final _PATERN = new RegExp(r"^(\-?\d+)(\.\d\d*)?$");
+final _0 = new Decimal.fromInt(0);
+final _1 = new Decimal.fromInt(1);
+final _5 = new Decimal.fromInt(5);
+final _10 = new Decimal.fromInt(10);
 
 Decimal dec(String value) => new Decimal(value);
 
@@ -28,7 +32,6 @@ int _gcd(int a, int b) {
   }
   return a;
 }
-int _abs(int a) => a < 0 ? -a : a;
 
 class Decimal implements Comparable {
   int _numerator, _denominator;
@@ -40,28 +43,42 @@ class Decimal implements Comparable {
     final group2 = match.group(2);
 
     if (group2 != null) {
-      int down = 1;
+      int denominator = 1;
       for (int i = 1; i < group2.length; i++) {
-        down = down * 10;
+        denominator = denominator * 10;
       }
-      _numerator = int.parse('${group1}${group2.substring(1)}');
-      _denominator = down;
+      _init(int.parse('${group1}${group2.substring(1)}'), denominator);
     } else {
-      _numerator = int.parse(group1);
-      _denominator = 1;
+      _init(int.parse(group1), 1);
     }
   }
   Decimal.fromFraction(int numerator, int denominator) {
+    _init(numerator, denominator);
+  }
+  Decimal.fromInt(int value) {
+    _init(value, 1);
+  }
+
+  void _init(int numerator, int denominator) {
     if (denominator == 0) throw new IntegerDivisionByZeroException();
-    int gcd = _gcd(_abs(numerator), _abs(denominator));
-    if (denominator < 0) {
-      this._numerator = -numerator ~/ gcd;
-      this._denominator = -denominator ~/ gcd;
+    if (numerator == 0) {
+      this._numerator = 0;
+      this._denominator = 1;
     } else {
-      this._numerator = numerator ~/ gcd;
-      this._denominator = denominator ~/ gcd;
+      final gcd = _gcd(numerator.abs(), denominator.abs());
+      if (denominator < 0) {
+        this._numerator = -numerator ~/ gcd;
+        this._denominator = -denominator ~/ gcd;
+      } else {
+        this._numerator = numerator ~/ gcd;
+        this._denominator = denominator ~/ gcd;
+      }
     }
   }
+
+  bool get isInteger => _denominator == 1;
+
+  bool operator ==(Decimal other) => this._numerator == other._numerator && this._denominator == other._denominator;
 
   String toStringAsFraction() => '${_numerator}/${_denominator}';
 
@@ -89,10 +106,88 @@ class Decimal implements Comparable {
   Decimal operator *(Decimal other) => new Decimal.fromFraction(this._numerator * other._numerator, this._denominator * other._denominator);
 
   /** Euclidean modulo operator. */
-  num operator %(num other) { throw 'unimplemented'; }
+  Decimal operator %(Decimal other) => this.remainder(other) + (isNegative ? other.abs() : _0);
 
   /** Division operator. */
   Decimal operator /(Decimal other) => new Decimal.fromFraction(this._numerator * other._denominator, this._denominator * other._numerator);
+
+  /**
+   * Truncating division operator.
+   *
+   * The result of the truncating division [:a ~/ b:] is equivalent to
+   * [:(a / b).truncate():].
+   */
+  Decimal operator ~/(Decimal other) => (this / other).truncate();
+
+  /** Negate operator. */
+  Decimal operator -() => new Decimal.fromFraction(-_numerator, _denominator);
+
+  /** Return the remainder from dividing this [num] by [other]. */
+  Decimal remainder(Decimal other) => this - (this ~/ other) * other;
+
+  /** Relational less than operator. */
+  bool operator <(Decimal other) => this.compareTo(other) < 0;
+
+  /** Relational less than or equal operator. */
+  bool operator <=(Decimal other) => this.compareTo(other) <= 0;
+
+  /** Relational greater than operator. */
+  bool operator >(Decimal other) => this.compareTo(other) > 0;
+
+  /** Relational greater than or equal operator. */
+  bool operator >=(Decimal other) => this.compareTo(other) >= 0;
+
+  bool get isNaN => false;
+
+  bool get isNegative => _numerator < 0;
+
+  bool get isInfinite => false;
+
+  /** Returns the absolute value of this [num]. */
+  Decimal abs() => isNegative ? (-this) : this;
+
+  /** Returns the greatest integer value no greater than this [num]. */
+  Decimal floor() => isNegative ? (this.truncate() - _1) : this.truncate();
+
+  /** Returns the least integer value that is no smaller than this [num]. */
+  Decimal ceil() => isNegative ? this.truncate() : (this.truncate() + _1);
+
+  /**
+   * Returns the integer value closest to this [num].
+   *
+   * Rounds away from zero when there is no closest integer:
+   *  [:(3.5).round() == 4:] and [:(-3.5).round() == -4:].
+   */
+  Decimal round() {
+    final abs = this.abs();
+    final absBy10 =  abs * _10;
+    Decimal dec;
+    if (absBy10 % _10 < _5) {
+      dec = abs.truncate();
+    } else {
+      dec = abs.truncate() + _1;
+    }
+    return isNegative ? -dec : dec;
+  }
+
+  /**
+   * Returns the integer value obtained by discarding any fractional
+   * digits from this [num].
+   */
+  Decimal truncate() => new Decimal.fromInt(this.toInt());
+
+  /** Truncates this [num] to an integer and returns the result as an [int]. */
+  int toInt() => _numerator ~/ _denominator;
+
+  /**
+   * Return this [num] as a [double].
+   *
+   * If the number is not representable as a [double], an
+   * approximation is returned. For numerically large integers, the
+   * approximation may be infinite.
+   */
+  double toDouble() => _numerator / _denominator;
+
 
   /**
    * Converts a [num] to a string representation with [fractionDigits]
@@ -118,4 +213,28 @@ class Decimal implements Comparable {
       return '${_numerator < 0 ? '-' : ''}$intPart.${fractionPart.toString().substring(1)}';
     }
   }
+
+  /**
+   * Converts a [num] to a string in decimal exponential notation with
+   * [fractionDigits] digits after the decimal point.
+   */
+  String toStringAsExponential(int fractionDigits)  => isInteger ? toInt().toStringAsExponential(fractionDigits) : toDouble().toStringAsExponential(fractionDigits);
+
+  /**
+   * Converts a [num] to a string representation with [precision]
+   * significant digits.
+   */
+  String toStringAsPrecision(int precision) => isInteger ? toInt().toStringAsPrecision(precision) : toDouble().toStringAsPrecision(precision);
+
+  /**
+   * Converts a [num] to a string representation in the given [radix].
+   *
+   * The [num] in converted to an [int] using [toInt]. That [int] is
+   * then converted to a string representation with the given
+   * [radix]. In the string representation, lower-case letters are
+   * used for digits above '9'.
+   *
+   * The [radix] argument must be an integer between 2 and 36.
+   */
+  String toRadixString(int radix) => toInt().toRadixString(radix);
 }
--- test/big_decimal_test.dart
+++ test/big_decimal_test.dart
@@ -2,55 +2,185 @@ import 'package:unittest/unittest.dart';
 import 'package:decimal/decimal.dart';
 
 main() {
-  test('decimal string validation', () {
+  test('string validation', () {
     expect(() => new Decimal('1'), returnsNormally);
     expect(() => new Decimal('-1'), returnsNormally);
     expect(() => new Decimal('1.'), throws);
     expect(() => new Decimal('1.0'), returnsNormally);
   });
-  test('decimal representation', () {
+  test('get isInteger', () {
+    expect(dec('1').isInteger, equals(true));
+    expect(dec('0').isInteger, equals(true));
+    expect(dec('-1').isInteger, equals(true));
+    expect(dec('-1.0').isInteger, equals(true));
+    expect(dec('1.2').isInteger, equals(false));
+    expect(dec('-1.21').isInteger, equals(false));
+  });
+  test('operator ==(Decimal other)', () {
+    expect(dec('1') == (dec('1')), equals(true));
+    expect(dec('1') == (dec('2')), equals(false));
+    expect(dec('1') == (dec('1.0')), equals(true));
+    expect(dec('1') == (dec('2.0')), equals(false));
+    expect(dec('1') != (dec('1')), equals(false));
+    expect(dec('1') != (dec('2')), equals(true));
+  });
+  test('toString()', () {
     ['0', '1', '-1', '-1.1', '23', '31878018903828899277492024491376690701584023926880.1'].forEach((String n) {
       expect(new Decimal(n).toString(), equals(n));
     });
   });
-  test('decimal toStringAsFixed', () {
-    [0, 1, 23, 2.2, 2.499999, 2.5, 2.7, 1.235].forEach((num n) {
-      [0, 1, 5, 10].forEach((p) {
-        expect(dec(n.toString()).toStringAsFixed(p), equals(n.toStringAsFixed(p)));
-      });
-    });
-  });
-  test('decimal comparaison', () {
+  test('compareTo(Decimal other)', () {
     expect(dec('1').compareTo(dec('1')), equals(0));
     expect(dec('1').compareTo(dec('1.0')), equals(0));
     expect(dec('1').compareTo(dec('1.1')), equals(-1));
     expect(dec('1').compareTo(dec('0.9')), equals(1));
   });
-  test('decimal addition', () {
+  test('operator +(Decimal other)', () {
     expect((dec('1') + dec('1')).toString(), equals('2'));
     expect((dec('1.1') + dec('1')).toString(), equals('2.1'));
     expect((dec('1.1') + dec('0.9')).toString(), equals('2'));
     expect((dec('31878018903828899277492024491376690701584023926880.0') + dec('0.9')).toString(), equals('31878018903828899277492024491376690701584023926880.9'));
   });
-  test('decimal soustraction', () {
+  test('operator -(Decimal other)', () {
     expect((dec('1') - dec('1')).toString(), equals('0'));
     expect((dec('1.1') - dec('1')).toString(), equals('0.1'));
     expect((dec('0.1') - dec('1.1')).toString(), equals('-1'));
     expect((dec('31878018903828899277492024491376690701584023926880.0') - dec('0.9')).toString(), equals('31878018903828899277492024491376690701584023926879.1'));
   });
-  test('decimal multiplication', () {
+  test('operator *(Decimal other)', () {
     expect((dec('1') * dec('1')).toString(), equals('1'));
     expect((dec('1.1') * dec('1')).toString(), equals('1.1'));
     expect((dec('1.1') * dec('0.1')).toString(), equals('0.11'));
     expect((dec('1.1') * dec('0')).toString(), equals('0'));
     expect((dec('31878018903828899277492024491376690701584023926880.0') * dec('10')).toString(), equals('318780189038288992774920244913766907015840239268800'));
   });
-  test('decimal division', () {
+  test('operator %(Decimal other)', () {
+    expect((dec('2') % dec('1')).toString(), equals('0'));
+    expect((dec('0') % dec('1')).toString(), equals('0'));
+    expect((dec('8.9') % dec('1.1')).toString(), equals('0.1'));
+    expect((dec('-1.2') % dec('0.5')).toString(), equals('0.3'));
+    expect((dec('-1.2') % dec('-0.5')).toString(), equals('0.3'));
+  });
+  test('operator /(Decimal other)', () {
     expect(() => dec('1') / dec('0'), throws);
     expect((dec('1') / dec('1')).toString(), equals('1'));
     expect((dec('1.1') / dec('1')).toString(), equals('1.1'));
     expect((dec('1.1') / dec('0.1')).toString(), equals('11'));
     expect((dec('0') / dec('0.2315')).toString(), equals('0'));
     expect((dec('31878018903828899277492024491376690701584023926880.0') / dec('10')).toString(), equals('3187801890382889927749202449137669070158402392688'));
+  });
+  test('operator ~/(Decimal other)', () {
+    expect(() => dec('1') ~/ dec('0'), throws);
+    expect((dec('3') ~/ dec('2')).toString(), equals('1'));
+    expect((dec('1.1') ~/ dec('1')).toString(), equals('1'));
+    expect((dec('1.1') ~/ dec('0.1')).toString(), equals('11'));
+    expect((dec('0') ~/ dec('0.2315')).toString(), equals('0'));
+  });
+  test('operator -()', () {
+    expect((-dec('1')).toString(), equals('-1'));
+    expect((-dec('-1')).toString(), equals('1'));
+  });
+  test('remainder(Decimal other)', () {
+    expect((dec('2').remainder(dec('1'))).toString(), equals('0'));
+    expect((dec('0').remainder(dec('1'))).toString(), equals('0'));
+    expect((dec('8.9').remainder(dec('1.1'))).toString(), equals('0.1'));
+    expect((dec('-1.2').remainder(dec('0.5'))).toString(), equals('-0.2'));
+    expect((dec('-1.2').remainder(dec('-0.5'))).toString(), equals('-0.2'));
+  });
+  test('operator <(Decimal other)', () {
+    expect(dec('1') < dec('1'), equals(false));
+    expect(dec('1') < dec('1.0'), equals(false));
+    expect(dec('1') < dec('1.1'), equals(true));
+    expect(dec('1') < dec('0.9'), equals(false));
+  });
+  test('operator <=(Decimal other)', () {
+    expect(dec('1') <= dec('1'), equals(true));
+    expect(dec('1') <= dec('1.0'), equals(true));
+    expect(dec('1') <= dec('1.1'), equals(true));
+    expect(dec('1') <= dec('0.9'), equals(false));
+  });
+  test('operator >(Decimal other)', () {
+    expect(dec('1') > dec('1'), equals(false));
+    expect(dec('1') > dec('1.0'), equals(false));
+    expect(dec('1') > dec('1.1'), equals(false));
+    expect(dec('1') > dec('0.9'), equals(true));
+  });
+  test('operator >=(Decimal other)', () {
+    expect(dec('1') >= dec('1'), equals(true));
+    expect(dec('1') >= dec('1.0'), equals(true));
+    expect(dec('1') >= dec('1.1'), equals(false));
+    expect(dec('1') >= dec('0.9'), equals(true));
+  });
+  test('get isNaN', () {
+    expect(dec('1').isNaN, equals(false));
+  });
+  test('get isNegative', () {
+    expect(dec('-1').isNegative, equals(true));
+    expect(dec('0').isNegative, equals(false));
+    expect(dec('1').isNegative, equals(false));
+  });
+  test('get isInfinite', () {
+    expect(dec('1').isInfinite, equals(false));
+  });
+  test('abs()', () {
+    expect((dec('-1.49').abs()).toString(), equals('1.49'));
+    expect((dec('1.498').abs()).toString(), equals('1.498'));
+  });
+  test('floor()', () {
+    expect((dec('-1.49').floor()).toString(), equals('-2'));
+    expect((dec('1.49').floor()).toString(), equals('1'));
+  });
+  test('ceil()', () {
+    expect((dec('-1.49').ceil()).toString(), equals('-1'));
+    expect((dec('1.49').ceil()).toString(), equals('2'));
+  });
+  test('round()', () {
+    expect((dec('1.4999').round()).toString(), equals('1'));
+    expect((dec('2.5').round()).toString(), equals('3'));
+    expect((dec('-2.51').round()).toString(), equals('-3'));
+    expect((dec('-2').round()).toString(), equals('-2'));
+  });
+  test('truncate()', () {
+    expect((dec('2.51').truncate()).toString(), equals('2'));
+    expect((dec('-2.51').truncate()).toString(), equals('-2'));
+    expect((dec('-2').truncate()).toString(), equals('-2'));
+  });
+  test('toInt()', () {
+    expect(dec('2.51').toInt(), equals(2));
+    expect(dec('-2.51').toInt(), equals(-2));
+    expect(dec('-2').toInt(), equals(-2));
+  });
+  test('toDouble()', () {
+    expect(dec('2.51').toDouble(), equals(2.51));
+    expect(dec('-2.51').toDouble(), equals(-2.51));
+    expect(dec('-2').toDouble(), equals(-2.0));
+  });
+  test('toStringAsFixed(int fractionDigits)', () {
+    [0, 1, 23, 2.2, 2.499999, 2.5, 2.7, 1.235].forEach((num n) {
+      [0, 1, 5, 10].forEach((p) {
+        expect(dec(n.toString()).toStringAsFixed(p), equals(n.toStringAsFixed(p)));
+      });
+    });
+  });
+  test('toStringAsExponential(int fractionDigits)', () {
+    [0, 1, 23, 2.2, 2.499999, 2.5, 2.7, 1.235].forEach((num n) {
+      [1, 5, 10].forEach((p) {
+        expect(dec(n.toString()).toStringAsExponential(p), equals(n.toStringAsExponential(p)));
+      });
+    });
+  });
+  test('toStringAsPrecision(int precision)', () {
+    [0, 1, 23, 2.2, 2.499999, 2.5, 2.7, 1.235].forEach((num n) {
+      [1, 5, 10].forEach((p) {
+        expect(dec(n.toString()).toStringAsPrecision(p), equals(n.toStringAsPrecision(p)));
+      });
+    });
+  });
+  test('toRadixString(int radix)', () {
+    [0, 1, 23, 2.2, 2.499999, 2.5, 2.7, 1.235].forEach((num n) {
+      [2, 5, 10, 36].forEach((p) {
+        expect(dec(n.toString()).toRadixString(p), equals(n.toRadixString(p)));
+      });
+    });
   });
 }
